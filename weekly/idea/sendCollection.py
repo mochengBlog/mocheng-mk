df7abd7cde8bceb443a408c8e6cd740f65a7c42c
liushuo <liushuo@7moor.com>
2024-09-05 23:49:36 +0800

    Deployed 74d6cd0 with MkDocs version: 1.6.0

--- weekly/idea/sendCollection.py
+++ weekly/idea/sendCollection.py
@@ -7,17 +7,16 @@ from email.header import Header
 # 第三方 SMTP 服务
 mail_host = "smtp.163.com"  # 设置服务器
 mail_user = "llmoyueqingcheng@163.com"  # 邮箱用户名
-mail_pass = "BBUKVHYMWKSWNXNA"  # 是邮箱授权口令，不是邮箱登录密码
+mail_pass = ""  # 是邮箱授权口令，不是邮箱登录密码
 
 sender = "llmoyueqingcheng@163.com"  # 发送邮件邮箱
 receivers = ["todo@mail.dida365.com"]  # 接收邮件，可添加多个邮箱
 
-message = MIMEText('测试0826...', 'plain', 'utf-8')  # 发送邮件的内容、类型
+message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')  # 发送邮件的内容、类型
 message['From'] = sender  # 邮件发信人，也可以自己定义，建议和发件人一致
 message['To'] = receivers[0]  # 邮件收件人，可自己定义，建议和收件人一致
 
-#时间
-subject = 'Python SMTP 邮件测试 测试 待整理'
+subject = 'Python SMTP 邮件测试'
 message['Subject'] = Header(subject, 'utf-8')
 
 try:
